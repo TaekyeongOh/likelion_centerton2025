cc7fc96ee0c7ba1f1b82399f719cc6e43feed6fd
kimseongyeon <seongyeon0810@naver.com>
2025-08-10 23:12:40 +0900

    Customer/Owner 폴더 분리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Main from './components/Main'
+import Main from './components/Customer_Section/Main'
 
 const App = () => {
   return (
