2c0aef523149dc06111d71bd26076751a54f8f62
soyu <soyou5876@naver.com>
2025-08-13 02:15:44 +0900

    손님: order/options 디자인 완료

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Main from './components/Customer_Section/Main'
+import Cus_options from './components/Customer_Section/Cus_options'
+import Cus_order from './components/Customer_Section/Cus_order'
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Main />} />
+      <Route path='/cus_order' element={<Cus_order />} />
+      <Route path='/cus_options' element={<Cus_options />} />
     </Routes>
     </BrowserRouter>
   )
