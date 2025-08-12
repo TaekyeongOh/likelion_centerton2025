6eea759e2a64c77a88ee7d51589b42a84f576f00
rlatjddus123 <seongyeon0810@naver.com>
2025-08-13 02:22:52 +0900

    Revert "프론트 1차 작업"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Menu from './components/Customer_Section/Menu'
-import Menu_Best from './components/Customer_Section/Menu_Best'
-import Menu_Language from './components/Customer_Section/Menu_Language'
+import Main from './components/Customer_Section/Main'
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Menu />} />
-      <Route path='/menu_best' element={<Menu_Best />} />
-      <Route path='/menu_language' element={<Menu_Language />} />
+      <Route path='/' element={<Main />} />
     </Routes>
     </BrowserRouter>
   )
---
+++ src/components/Customer_Section/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div id="Main_wrap" className="container">
+        
+    </div>
+  )
+}
+
+export default Main
