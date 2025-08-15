2da9397939790f910ee3bd014bda20a06f1473c3
kimseongyeon <seongyeon0810@naver.com>
2025-08-16 00:53:24 +0900

    index.js 에 Owner_App.js 경로 추가

---
+++ src/components/Owner_Section/Menu_Edit.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import back from '../../assets/img/owner_menu_edit/back.svg'
+import qr from '../../assets/img/owner_menu_edit/qr.svg'
+
+const Menu_Edit = () => {
+  return (
+    <div id="Menu_Edit_Wrap" className="container">
+        <header>
+            <img src={back} alt="" />
+            <img src={qr} alt="" />
+        </header>
+        <main>
+
+        </main>
+    </div>
+  )
+}
+
+export default Menu_Edit
