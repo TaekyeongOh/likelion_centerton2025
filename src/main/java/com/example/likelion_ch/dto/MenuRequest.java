d3012df18d1d8849632c4218287abad7a64a2813
TaekyeongOhMe <161999984+TaekyeongOh@users.noreply.github.com>
2025-08-19 15:08:24 +0900

    Feat : 손님 화면에 가게 정보 나타나도록 구현

--- src/main/java/com/example/likelion_ch/dto/MenuRequest.java
+++ src/main/java/com/example/likelion_ch/dto/MenuRequest.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.math.BigDecimal;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class MenuRequest {
